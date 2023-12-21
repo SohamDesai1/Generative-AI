b2e5bd38ee873031a4b7efe6a8fc52692e82ac06
SohamDesai1 <sohamdesai123@gmail.com>
2023-12-21 12:40:58 +0530

    down graded to 7B model

--- llama-7B.py
+++ llama-7B.py
@@ -7,7 +7,7 @@ from langchain.llms import CTransformers
 def getLLamaresponse(input_text,no_words,blog_style):
 
     ### LLama2 model
-    llm=CTransformers(model='models/llama-2-13b-chat.ggmlv3.q8_0.bin',
+    llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
                       model_type='llama',
                       config={'max_new_tokens':256,
                               'temperature':0.01})
