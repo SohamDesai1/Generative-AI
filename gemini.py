df0f6db85dbd3d7f5d1cc9be3536e72be1ea4741
SohamDesai1 <sohamdesai123@gmail.com>
2023-12-23 13:50:49 +0530

    feat: added image-2-text with vision model

--- gemini.py
+++ gemini.py
@@ -2,18 +2,36 @@ from dotenv import load_dotenv
 import os
 import google.generativeai as genai
 import streamlit as st
+import PIL.Image
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
-model = genai.GenerativeModel('gemini-pro')
-def generate_content(prompt):
-    response = model.generate_content(prompt)
+model_text = genai.GenerativeModel('gemini-pro')
+model_image = genai.GenerativeModel('gemini-pro-vision')
+
+def generate_text(prompt):
+    response = model_text.generate_content(prompt)
+    return response.text
+
+def generate_image(prompt):
+    response = model_image.generate_content(prompt)
     return response.text
 
 st.title('Gemini AI Text Generator')
-prompt = st.text_input('Enter a prompt:')
-if st.button('Generate'):
-    response = generate_content(prompt)
-    st.write(response)
+
+text,image = st.tabs(['Text','Image'])
+
+with text:
+    prompt = st.text_input('Enter a prompt:')
+    if st.button('Generate'):
+        response = generate_text(prompt)
+        st.write(response)
+        
+with image: 
+    image = st.file_uploader(label='Upload an image',type=['jpg', 'jpeg', 'png'])
+    if st.button('Generate Text from Image'):
+        img = PIL.Image.open(image)
+        response = generate_image(img)
+        st.write(response)
