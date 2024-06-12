38e06f5baf6ebd5d5823414d8d598ab19ca9fc2b
SohamDesai1 <sohamdesai123@gmail.com>
2024-06-12 11:34:44 +0530

    learning mesop a new UI framework for python

---
+++ learn_mesop.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+import mesop as me
+import mesop.labs as mel
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
+
+@me.page(path='/', title="Chatbot")
+def app():
+  mel.chat(transform=transform,title="Gemini Chat")
+
+
+def transform(input:str , history: list[mel.ChatMessage]):
+    response = model.generate_content(input)
+    yield response.text
