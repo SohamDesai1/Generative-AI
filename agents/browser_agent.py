f4b44fe6044b78f031316257ece70f5433381177
SohamDesai1 <sohamdesai123@gmail.com>
2025-01-12 17:23:20 +0530

    fix: :zap: updated to gemini llm

--- agents/browser_agent.py
+++ agents/browser_agent.py
@@ -1,28 +1,26 @@
 import os
 from dotenv import load_dotenv
-from langchain_openai import AzureChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from browser_use import Agent
 from pydantic import SecretStr
 import asyncio
 
 load_dotenv()
 
-llm = AzureChatOpenAI(
-    azure_deployment="gpt-4",
-    api_key=SecretStr(os.getenv("AZURE_OPENAI_API_KEY")),
-    api_version="2024-05-01-preview",
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    rate_limiter=None,
+llm2 = ChatGoogleGenerativeAI(
+    api_key=SecretStr(os.getenv("GOOGLE_API_KEY")),
+    model="gemini-2.0-flash-exp"   
 )
 
 agent = Agent(
     task="Go to amazon.com, search for laptop, sort by best rating, and give me the price of the first result",
-    llm=llm,
+    llm=llm2,
+    generate_gif=False
 )
 
 
 async def main():
-    await agent.run(max_steps=3)
+    await agent.run()
 
 
 asyncio.run(main())
